14f822e74a4447325876e1241c2249585f4c5d07
Sascha Hahne <ntbm.morulia@gmail.com>
2017-11-28 22:28:18 +0100

    some more beauty

--- src/index.js
+++ src/index.js
@@ -17,6 +17,32 @@ function createNodesAndDataset (data) {
     edges: new vis.DataSet(edgesArray)
   }
 }
+function nodeColor (parent_relationship) {
+  switch (parent_relationship) {
+    case "contradicts":
+      return "red"
+    case "supports":
+      return "green"
+    case "maybe_supports":
+      return "green"
+    case "inherit":
+      return null
+    default:
+      console.error("invalid relationship")
+      return null
+  }
+}
+function nodeShape (type) {
+  switch (type) {
+    case "concept":
+      return "box"
+    case "term":
+      return "ellipse"
+    default:
+      console.error("invalid type")
+      return null
+  }
+}
 function a (parent, current_node, nodesArray, edgesArray) {
   let {name, type="concept", parent_relationship="inherit", meta, children} = current_node
   let current = {
@@ -33,6 +59,8 @@ function a (parent, current_node, nodesArray, edgesArray) {
       to: current.id
     })
   }
+  current.color = nodeColor(current.parent_relationship)
+  current.shape = nodeShape(type)
   nodesArray.push(current)
   if(current_node.children){
     for(let node of current_node.children){
