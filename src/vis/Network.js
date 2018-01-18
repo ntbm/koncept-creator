433599950aa5b3bdd4dc6dfaa5445103580c341c
Sascha Hahne <ntbm.morulia@gmail.com>
2018-01-18 18:02:31 +0100

    FEAT: remove, disable internal ui

--- src/vis/Network.js
+++ src/vis/Network.js
@@ -4,12 +4,17 @@ class Network extends vis.Network {
   constructor (container, data, visOptions) {
     super(container, data, visOptions)
     this.getAllNodesAndEdges = getAllNodesAndEdges.bind(this)
+    this.clear = clear.bind(this)
   }
-
 }
 function getAllNodesAndEdges () {
   let {nodes, edges} = this.body.data
   return {nodes, edges}
 }
-
+function clear () {
+  this.setData({
+    nodes: new vis.DataSet([]),
+    edges: new vis.DataSet([])
+  })
+}
 module.exports = Network
